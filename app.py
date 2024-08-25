b5c465e05edb2415d1289e33691e55b19dbbf7d3
Arpit Tyagi <134762653+arpittyagi-19@users.noreply.github.com>
2024-08-25 20:35:57 +0530

    app.py

--- app.py
+++ app.py
@@ -11,4 +11,6 @@ def index():
     return render_template('Untitled.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Ensure your Flask app listens on the correct host and port
+    port = int(os.environ.get("PORT", 10000))  # Use the PORT environment variable provided by Render
+    app.run(host="0.0.0.0", port=port)
